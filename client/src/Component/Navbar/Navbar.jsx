b51a1117da1c1194a75f158a91544d18c69514e6
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-23 12:42:31 +1100

    update

--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -7,6 +7,8 @@ function Navbar() {
   const [smallnav, setsmallnav] = useState(false)
   const [nav, setnav] = useState(true)
   const [classes, setclasses] = useState("")
+  const [overlay, setoverlay] = useState("")
+
 
 
 
@@ -38,9 +40,13 @@ window.addEventListener('scroll', changenavbar);
 useEffect(()=>{
   if(nav === true && window.innerWidth<515){
     setclasses("second hide")
+    setoverlay("overlay hide")
+
   }
   else{
     setclasses("second")
+    setoverlay("overlay")
+
   }
 },[nav])
 
@@ -63,7 +69,7 @@ useEffect(()=>{
            <div><span className='item7'>|</span></div>
            <div className='searchbox'><svg id="Capa_1" enable-background="new 0 0 515.558 515.558" height="24" viewBox="0 0 515.558 515.558" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z"></path></svg></div>
         </div>
-        <div  className='overlay'></div>
+        <div  className={overlay}></div>
 
         <div className='icon' style={{cursor:'pointer'}} ><DehazeIcon fontSize='large' onClick={()=>(setnav((prev)=>!prev))}/></div>
 
