464f8b7347ab3ea87f01f8e7b780ba881a7f8198
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-23 11:34:19 +1100

    update

--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React, {useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import './Navbar.scss'
 import logo from '../../Assets/Img/logo.png'
 import DehazeIcon from '@mui/icons-material/Dehaze';
 function Navbar() {
   const [isdisplayed, setdisplayed] = useState(false)
   const [smallnav, setsmallnav] = useState(false)
+  const [nav, setnav] = useState(false)
+  const [classes, setclasses] = useState("")
+
+
 
 
   const [crr,setcrr] = useState(0);
@@ -29,10 +33,18 @@ const changenavbar = ()=>{
 
 setcrr(window.scrollY)
 }
+window.addEventListener('scroll', changenavbar);
 
-window.addEventListener('scroll', changenavbar)
+useEffect(()=>{
+  if(nav === true && window.innerWidth<515){
+    setclasses("second hide")
+  }
+  else{
+    setclasses("second")
+  }
+},[nav])
 
-  
+
 
   return (
 
@@ -41,7 +53,7 @@ window.addEventListener('scroll', changenavbar)
             <img src={logo} alt=''/>
         </div>
 
-        <div className='second'> 
+        <div className={classes}> 
             <span className='item1'>Our work</span>
            <span className='item2'>Services</span>
            <span className='item3'>About</span>           
@@ -52,7 +64,7 @@ window.addEventListener('scroll', changenavbar)
            <div className='searchbox'><svg id="Capa_1" enable-background="new 0 0 515.558 515.558" height="24" viewBox="0 0 515.558 515.558" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z"></path></svg></div>
         </div>
 
-        <div className='icon' style={{cursor:'pointer'}}><DehazeIcon fontSize='large'/></div>
+        <div className='icon' style={{cursor:'pointer'}} ><DehazeIcon fontSize='large' onClick={()=>(setnav((prev)=>!prev))}/></div>
 
     </div>
   )
