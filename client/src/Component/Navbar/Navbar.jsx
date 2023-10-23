061121cba6dd07963df79169afe0fdb1e197461c
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-24 00:09:39 +1100

    Update Navbar.jsx

--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -48,7 +48,16 @@ useEffect(()=>{
     setoverlay("overlay")
 
   }
+  
+  
+
 },[nav])
+
+const as = ()=>{
+  setnav(!nav)
+}
+
+console.log(nav);
 
 
 
@@ -60,12 +69,12 @@ useEffect(()=>{
         </div>
 
         <div className={classes}> 
-            <span className='item1'>Our work</span>
-           <span className='item2'>Services</span>
-           <span className='item3'>About</span>           
-           <span className='item4'>Blog</span>
-           <span className='item5'>Carerres</span>
-           <span className='item6'>Contact</span>
+            <span className='item1' onClick={as}>Our work</span>
+           <span className='item2' onClick={as}>Services</span>
+           <span className='item3' onClick={as}>About</span>           
+           <span className='item4' onClick={as}>Blog</span>
+           <span className='item5' onClick={as}>Carerres</span>
+           <span className='item6' onClick={as}>Contact</span>
            <div><span className='item7'>|</span></div>
            <div className='searchbox'><svg id="Capa_1" enable-background="new 0 0 515.558 515.558" height="24" viewBox="0 0 515.558 515.558" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z"></path></svg></div>
         </div>
