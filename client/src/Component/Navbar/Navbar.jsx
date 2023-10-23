6f8ba1ffae59c2a73f22524a3526d9d9d9cb1cf5
Nischal Shrestha <nischalstha14@gmail.com>
2023-10-23 11:54:28 +1100

    UPDATE

--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import DehazeIcon from '@mui/icons-material/Dehaze';
 function Navbar() {
   const [isdisplayed, setdisplayed] = useState(false)
   const [smallnav, setsmallnav] = useState(false)
-  const [nav, setnav] = useState(false)
+  const [nav, setnav] = useState(true)
   const [classes, setclasses] = useState("")
 
 
@@ -63,6 +63,7 @@ useEffect(()=>{
            <div><span className='item7'>|</span></div>
            <div className='searchbox'><svg id="Capa_1" enable-background="new 0 0 515.558 515.558" height="24" viewBox="0 0 515.558 515.558" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z"></path></svg></div>
         </div>
+        <div  className='overlay'></div>
 
         <div className='icon' style={{cursor:'pointer'}} ><DehazeIcon fontSize='large' onClick={()=>(setnav((prev)=>!prev))}/></div>
 
